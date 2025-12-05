284fb419e3180c17d81cedc5250eb5e0633623b7
Lakshya Mittal <lakshyamittalaka@gmail.com>
2025-12-06 00:25:05 +0530

    Adjust pipe distance

--- src/constants.js
+++ src/constants.js
@@ -5,9 +5,9 @@ export const GROUND_HEIGHT = 26
 // Pipe tuning
 export const PIPE_SPEED = 80 // px/s
 export const PIPE_SPAWN_INTERVAL = 1.6 // seconds
-// Increased gap by ~30% for more breathing room at start
-export const PIPE_GAP_MIN = 83
-export const PIPE_GAP_MAX = 109
+// Wider gaps for easier navigation
+export const PIPE_GAP_MIN = 85
+export const PIPE_GAP_MAX = 120
 // Smaller margin to allow more vertical variation of gap centers
 export const PIPE_VERTICAL_MARGIN = 16
 export const PIPE_SCORE_OFFSET = 10 // pixels before pipe end to award score
